19574a97509e17f2f8ed0d04a112580a4d10c216
Frank Aiello <frankvaiello@gmail.com>
2022-11-11 12:04:08 -0500

    WIP - Adds layers to map

--- new-map.js
+++ new-map.js
@@ -19,3 +19,23 @@ const imageUrl = 'Saug-PhotoArchive.jpg';
 const latLngBounds = L.latLngBounds([southWest, northEast]);
 
 const imageOverlay = L.imageOverlay(imageUrl, latLngBounds).addTo(map);
+
+// Test getting the center of the map using pixel coordinates
+// const center = latLngBounds.getCenter();
+// console.log(center); // => {lat: -688.5, lng: 400}
+// Appears that y value is negative, to convert from image map x/y coords
+// const imgMapCenter = L.point(WIDTH/2,HEIGHT/2);
+// const transformation = L.transformation(1, 0, -1, 0);
+// const transformed = transformation.transform(imgMapCenter);
+// const testMarker = L.marker([transformed.y, transformed.x]).addTo(map);
+
+const transformation = L.transformation(1, 0, -1, 0);
+
+JSON.parse(layers).forEach((layer) => {
+  const point = L.point(layer.coords);
+  const transformed = transformation.transform(point);
+  const marker = L.marker([transformed.y, transformed.x]).addTo(map);
+  marker.on('click', () => {
+    window.location.href = layer.href;
+  })
+})
--- parse-map.js
+++ parse-map.js
@@ -9,10 +9,51 @@ function parseFile(html) {
   const $ = cheerio.load(html);
 
   $('area').each(function() {
-    console.log($(this).attr('coords'));
+    // link handling
+    let href = $(this).attr('href');
+    if (href) {
+      const linkMatch = /http:\/\/sdhistoricalsociety/.test(href);
+      if (linkMatch) {
+        // replace .org with .net
+        href = href.replace('sdhistoricalsociety.org', 'sdhistoricalsociety.net');
+      } else {
+        return; // if no match, then don't collect it
+      }
+    }
+    // shape parsing
+    const shape = $(this).attr('shape');
+    let coords = $(this).attr('coords').split(',');
+    if (shape === 'circle') {
+      coords = handleCircle(coords);
+    } else if (shape === 'rect') {
+      coords = handleRect(coords);
+    } else {
+      return;
+    }
+    // title
+    // let title = $(this).attr('title');
+    // if (title) {
+    //   title = title.replace(/'/g, "\'");
+    // }
+    layers.push({ coords, href });
   })
+  const newLayersJson = JSON.stringify(layers);
+  const newLayers = `const layers = '${newLayersJson}'`;
+  fs.writeFile('new-layers.js', newLayers, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
-  // process.stdout.write($.html());
+function handleCircle([x,y,radius]) {
+  return [parseInt(x), parseInt(y)];
+}
+
+function handleRect([x1,y1,x2,y2]) {
+  const height = parseInt(y2 - y1);
+  const width = parseInt(x2 - x1);
+  return [parseInt(x1)+(height/2),parseInt(y1)+(width/2)];
 }
 
 fs.readFile('index.html','utf8', (err, data) => {
